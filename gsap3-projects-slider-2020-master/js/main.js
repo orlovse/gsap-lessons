3738d5555d7453cfed1a17ce9429b8659f455928
orlovse <hajoff@gmail.com>
2021-02-15 14:10:33 +0200

    add timeline

--- gsap3-projects-slider-2020-master/js/main.js
+++ gsap3-projects-slider-2020-master/js/main.js
@@ -1,6 +1,29 @@
 function init() {
   gsap.set(".projects", { autoAlpha: 1 });
   gsap.set(".project", { x: "-100%" });
+
+  const element = document.querySelector(".project.project01");
+  const projectClasses = element.className.split(" ");
+  const projectClass = projectClasses[1];
+  const updateClass = (projectClass) => {
+    document.querySelector("body").className = projectClass;
+  };
+
+  const tlIn = gsap.timeline();
+  tlIn.fromTo(
+    element,
+    {
+      autoAlpha: 0,
+      x: "-100%",
+    },
+    {
+      duration: 0.7,
+      x: 0,
+      autoAlpha: 1,
+      onStart: updateClass,
+      onStartParams: [projectClass],
+    }
+  );
 }
 
 window.addEventListener("load", function () {
