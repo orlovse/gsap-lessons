969a580223a8058ece18d1156721a4fc74007244
orlovse <hajoff@gmail.com>
2021-02-15 14:27:15 +0200

    add timeline creators

--- gsap3-projects-slider-2020-master/js/main.js
+++ gsap3-projects-slider-2020-master/js/main.js
@@ -2,34 +2,48 @@ function init() {
   gsap.set(".projects", { autoAlpha: 1 });
   gsap.set(".project", { x: "-100%" });
 
-  const element = document.querySelector(".project.project01");
-  const projectClasses = element.className.split(" ");
-  const projectClass = projectClasses[1];
   const updateClass = (projectClass) => {
     document.querySelector("body").className = projectClass;
   };
 
-  const tlIn = gsap.timeline();
-  tlIn.fromTo(
-    element,
-    {
-      autoAlpha: 0,
-      x: "-100%",
-    },
-    {
-      duration: 0.7,
-      x: 0,
-      autoAlpha: 1,
-      onStart: updateClass,
-      onStartParams: [projectClass],
-    }
-  );
+  const createTimelineIn = (index) => {
+    const element = document.querySelector(".project.project0" + index);
+    const projectClasses = element.className.split(" ");
+    const projectClass = projectClasses[1];
+    const tlIn = gsap.timeline();
+    tlIn.fromTo(
+      element,
+      {
+        autoAlpha: 0,
+        x: "-100%",
+      },
+      {
+        duration: 0.7,
+        x: 0,
+        autoAlpha: 1,
+        onStart: updateClass,
+        onStartParams: [projectClass],
+      }
+    );
 
-  const tlOut = gsap.timeline();
-  tlOut.to(element, { duration: 0.7, x: 250, autoAlpha: 0 });
+    return tlIn;
+  };
 
-  const tlTransition = gsap.timeline();
-  tlTransition.add(tlIn).add(tlOut);
+  const createTimelineOut = (index) => {
+    const tlOut = gsap.timeline();
+    tlOut.to(element, { duration: 0.7, x: 250, autoAlpha: 0 });
+    return tlOut;
+  };
+
+  const createTimelineTransition = () => {
+    const tlTransition = gsap.timeline();
+    tlTransition.add(tlIn).add(tlOut);
+    return tlTransition;
+  };
+
+  let currentStep = 1;
+
+  createTimelineIn(currentStep);
 }
 
 window.addEventListener("load", function () {
