904b8122a3faf9b39cf49098a9e0e7170afd3229
orlovse <hajoff@gmail.com>
2021-03-12 20:06:05 +0200

    add pen scroll animation

--- gsap3-pen-reveal-2020-master/js/main.js
+++ gsap3-pen-reveal-2020-master/js/main.js
@@ -1,9 +1,37 @@
-function init(){
-    
-    // start here
+function init() {
+  const height = document.querySelector(".pen-top").clientHeight - 22;
+  gsap.set(".part3", {
+    y: () => {
+      return -height;
+    },
+    scrollTrigger: {
+      id: "pen-body",
+      trigger: ".part3",
+      start: "top bottom-=270px",
+      end: `+=${height}`,
+      pin: true,
+      pinSpacing: false,
+    },
+  });
 
+  const partTopOffsets = [547, 722, 842];
+
+  gsap.utils.toArray([".part4", ".part5", ".part6"]).forEach((part, index) => {
+    gsap.set(part, { y: -partTopOffsets[index] });
+
+    gsap.to(part, {
+      y: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".pen-body",
+        start: "top bottom-=640px",
+        end: `+=${partTopOffsets[index]}`,
+        scrub: true,
+      },
+    });
+  });
 }
 
-window.addEventListener('load', function(){
-    init();
+window.addEventListener("load", function () {
+  init();
 });
